d6f301b71106423146409b56856f6082effa26fb
flying-turtle001 <justin.esposito@gmx.de>
2022-02-25 10:13:08 +0100

    add base styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import * as React from "react";
 
 const IndexPage = () => {
-  return <h1>Homepage</h1>;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        totam!
+      </p>
+    </div>
+  );
 };
 
 export default IndexPage;
