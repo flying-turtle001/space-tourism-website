55babdf1f01e63a131f52daeb7e4cedd1517c694
flying-turtle001 <justin.esposito@gmx.de>
2022-02-26 12:30:52 +0100

    create content + add styles to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 
+import { container, contentWrapper } from "../styles/index.module.css";
+
 import Wrapper from "../components/layout/Wrapper";
 import Header from "../components/layout/Header";
 
 const IndexPage = () => {
   return (
-    <div>
+    <div className={container}>
       <Header />
       <Wrapper>
-        <h1>Space</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-          totam!
-        </p>
+        <div className={contentWrapper}>
+          <h5>So, you want to travel to</h5>
+          <h1>Space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
       </Wrapper>
     </div>
   );
